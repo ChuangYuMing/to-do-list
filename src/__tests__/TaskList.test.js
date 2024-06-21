0471b72e7e23723d4ecaed790459a626182ba400
JamieChuang <chming28@gmail.com>
2024-06-21 12:41:00 +0800

    fix test

--- src/__tests__/TaskList.test.js
+++ src/__tests__/TaskList.test.js
@@ -4,8 +4,13 @@ import TaskList from "../components/task-list/TaskList";
 
 it("renders correctly", () => {
   const mockFilterTasks = [
-    { createdAt: 1, id: 1, description: "Task 1", completed: false },
-    { createdAt: 2, id: 2, description: "Task 2", completed: true },
+    {
+      createdAt: 1718944797228,
+      id: 1,
+      description: "Task 1",
+      completed: false,
+    },
+    { createdAt: 1718924797228, id: 2, description: "Task 2", completed: true },
   ];
 
   const { asFragment } = render(
